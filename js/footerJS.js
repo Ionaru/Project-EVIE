3b8bfe2ccf195602f9b6cacd282bb2d9cb663f7c
jeroen.akkerman <Jeroen_akkerman1@hotmail.com>
2016-02-15 12:32:52 +0100

    More work on the character-switcher v4

--- js/footerJS.js
+++ js/footerJS.js
@@ -21,18 +21,6 @@ jQuery(document).ready(function () {
         $("#imageCalendar").attr('src', 'icons/calendar.png');
         $("#imageSettings").attr('src', 'icons/settings.png');
     }
-    var i = 0;
-    while (i < charIDs.length) {
-        var css = "characterInactive";
-        if (i == selectedCharacter) {
-            css = "characterActive";
-            selectedCharacterID = charIDs[i];
-        }
-        $('#charLinks').css('visibility', 'visible').append('<li><a id="charLink' + i + '" class="' + css + '" href="?char=' + i + '"><img alt="char' + i + '" id="char' + i + '" style="max-height: 50px" class="img" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="50" height="50"></a></li>');
-        $('#char' + i).css('visibility', 'visible').attr('src', 'https://image.eveonline.com/Character/' + charIDs[i] + '_50.jpg');
-        $('#charmbl' + i).css('visibility', 'visible').attr('src', 'https://image.eveonline.com/Character/' + charIDs[i] + '_256.jpg');
-        i++;
-    }
 });
 
 //Get Character ID from a name
