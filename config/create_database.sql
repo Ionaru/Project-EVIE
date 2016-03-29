18707a94686b021df7acdd4b03bbc5222b8c9f61
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2016-03-29 17:13:15 +0200

    Updated the SQL script to match the example config files.

--- config/create_database.sql
+++ config/create_database.sql
@@ -6,8 +6,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `web_evie` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `web_evie`;
+CREATE DATABASE IF NOT EXISTS `evie_database` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `evie_database`;
 
 CREATE TABLE `apikeys` (
   `apikey_id` int(11) NOT NULL,
